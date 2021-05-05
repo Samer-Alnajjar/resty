421644ed0edfb32995cbec7a25c72dd262d33a27
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-05-05 23:34:35 +0300

    Finish lab29

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import './Header.scss'
+import {NavLink} from 'react-router-dom'
 
 const header = () => {
   return (
     <header>
       <h1>RESTy</h1>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/history">History</NavLink>
+        </li>
+        <li>
+          <NavLink to="/help">Help</NavLink>
+        </li>
+      </ul>
     </header>
   )
 }
---
+++ src/components/History/HistoryPage.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import { If, Then, Else } from '../If'; //'react-if'
+import ReactJson from 'react-json-view';
+
+
+class HistoryPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiMemoryData : JSON.parse(localStorage.getItem("API data")) || [],
+      body: []
+    }
+  }
+
+  handleAPI = (data) => {
+    this.setState({ body: data });
+  }
+
+  render() {
+    return (
+      <>
+        <If condition={this.state.apiMemoryData.length}>
+          <Then>
+            {this.state.apiMemoryData.map((apiData, index) => {
+              return (
+                <div onClick={() => {this.handleAPI(apiData)}} key={index}>
+                  {apiData.method} {apiData.URL}
+                </div>
+              )
+            })}
+          </Then>
+        </If>
+        <div id="api">
+          <If condition={!this.state.body || this.state.body.length === 0}>
+            <Then>
+              <div>No data available </div>
+            </Then>
+            <Else>
+              <ReactJson src={ this.state.body } name={false} theme="rjv-default" />
+            </Else>
+          </If>
+        </div>
+      </>
+    )
+  }
+
+}
+
+export default HistoryPage
